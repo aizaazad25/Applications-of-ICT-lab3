0acb3fcfcfae15ada99006d7e87a303e6a3a5c66
iyesha547-lilium <iyeshaimran@gmail.com>
2025-09-22 13:51:06 +0500

    Add files via upload

---
+++ Lab_03/logic.py
@@ -0,0 +1,17 @@
+marks = int(input("Enter your marks (0-100): "))
+
+if marks >= 90:
+    grade = "A"
+    print(grade)
+
+elif marks >= 75 and marks < 90:
+    grade = "B"
+    print(grade)
+
+elif marks >= 50 and marks < 75:
+    grade = "C"
+    print(grade)
+
+else: # marks < 50
+    grade = "F"
+    print(grade)
